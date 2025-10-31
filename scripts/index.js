8803ba841ec70cc448997a68ff24a24e040aa380
Márcio Nogueira <88255519+omarcionogueira@users.noreply.github.com>
2025-10-31 10:04:51 -0400

    Create index.js

---
+++ scripts/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './estilos/principal.css';
+
+const raiz = ReactDOM.createRoot(document.getElementById('root'));
+raiz.render(<App />);
